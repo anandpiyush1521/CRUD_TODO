bb5b56abac87bcb545fcd5a58eb890bfe46a9ff8
Piyush Anand <piyushanand2580@gmail.com>
2022-07-27 01:03:41 +0530

    secondary Commit

---
+++ TODO/src/MyComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export const Footer = () => {
+    return (
+        <footer classNamme="bg-dark text-light py-3">
+            <p classNamme="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
